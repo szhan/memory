ab67c1f96507d01fa76646a17c7d6858fa178f44
Shing Hei Zhan <szhan@Shings-MacBook-Pro.local>
2017-07-04 10:32:42 -0600

    Continue to clean up

--- analysis/benchmark/benchmarking.r
+++
@@ -1,7 +0,0 @@
-# bench marking CSSR
-rm(list = ls())
-
-require('CSSR')
-
-output <- runCSSR(alphabet="01", data="010101101010101", maxLength=15, 
-                  isChi=FALSE, sigLevel=0.001, outputPrefix="../output/test")
