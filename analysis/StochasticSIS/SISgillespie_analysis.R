21872324f52c95c95647a5921c1689fd73cdf131
Jake L. Weissman <jw4336@umd.edu>
2017-08-29 19:10:58 -0400

    added stochastic model with seasonal forcing, can give sustained cycles (stochastic amplification)

--- analysis/StochasticSIS/SISgillespie_analysis.R
+++ analysis/StochasticSIS/SISgillespie_analysis.R
@@ -1,12 +1,66 @@
 library(CSSR)
+library(adaptivetau)
 
-
-#setwd("/media/jlweissman/OS/Storage/Grad/LabNotebook/CSSS/Memory/memory")
+setwd("/media/jlweissman/OS/Storage/Grad/LabNotebook/CSSS/Memory/memory")
 source("./src/SIS_gillespie.R")
 source("./src/entropy_calc.R")
 source("./src/run_cssr_by_min_bic.R")
 
-params = list(z=1e-6, beta=1, gamma=1e-1, rho=1, alpha=1)
-sis_out <- SISg(params,sim_length=10000,S0=1e5,I0=0)
-plot(sis_out$time,sis_out$I)
-plot(sis_out$time,sis_out$S)
+#SISg() for adaptive tau-leaping algorithm (fast)
+#SISg.exact() for gillespie algorithm (slow)
+
+params = list(z=1e-6, beta=1e-5, gamma=1e-3, rho=1, alpha=1e-6)
+sis_out <- SISg(params,sim_length=10000,S0=800,I0=1)
+
+plot(sis_out$time,sis_out$S,col="blue",ylim=c(1,1e3),type="l")
+lines(sis_out$time,sis_out$I,col="red")
+
+params = list(z=1e-6, beta=5e-1, gamma=5e-2, rho=0.5, alpha=0,mu=1e-5)
+sirs_out <- SIRSg(params,sim_length=10000,S0=1e6,I0=1)
+
+plot(sirs_out$time,sirs_out$S,col="blue",ylim=c(1,1e6),type="l",log="y",xlim=c(0,1000))
+lines(sirs_out$time,sirs_out$I,col="red")
+lines(sirs_out$time,sirs_out$R,col="green")
+lines(sirs_out$time,sirs_out$B,col="magenta")
+
+#############
+# SIE Model #
+#############
+
+params = list(z=0, r=1.5, K=12, beta=0.8, m=0.5, epsilon=1e-1, tau=1e-2, rho=1)
+sie_out <- SIEg(params,sim_length=100,S0=params$K,I0=1,E0=0)
+
+par(mfrow=c(3,1))
+plot(sie_out$time,sie_out$S,col="blue",type="l")
+plot(sie_out$time,sie_out$I,col="red",type="l")
+plot(sie_out$time,sie_out$E,col="green",type="l")
+par(mfrow=c(1,1))
+
+params = list(z=0, r=1.5, K=3000, beta=0.1, m=0.567, epsilon=0.111+0.4, tau=1, rho=1)
+sie_out <- SIEg.exact(params,sim_length=100,S0=params$K,I0=5,E0=0)
+
+par(mfrow=c(3,1))
+plot(sie_out$time,sie_out$S,col="blue",type="l")
+plot(sie_out$time,sie_out$I,col="red",type="l")
+plot(sie_out$time,sie_out$E,col="green",type="l")
+par(mfrow=c(1,1))
+
+
+###########################################################################################
+# Seasonally forced stochastic SIR (Black and McKane 2010 Journal of Theoretical Biology) #
+###########################################################################################
+
+params <-  list(z=1e-6, beta0=1e-5, beta1=0.2, gamma=5e-2, rho=0.5, alpha=0,mu=1e-5)
+sirs_out <- SIRSg_forced(params,sim_iter=2000,on_interval=7,off_interval=3,S0=2e6,I0=1,R0=0)
+
+plot(sirs_out$time,sirs_out$S,col="blue",type="l",log="y")
+lines(sirs_out$time,sirs_out$I,col="red")
+lines(sirs_out$time,sirs_out$R,col="green")
+lines(sirs_out$time,sirs_out$B,col="magenta")
+
+plot(sirs_out$time,sirs_out$I,col="red",ylim=c(0,1000),type="l")
+
+
+
+
+
--- src/SIS_gillespie.R
+++ src/SIS_gillespie.R
@@ -11,6 +11,10 @@
 #library(adaptivetau)
 #params = list(z=1e-6, beta=1e-5, mu=1e-2, gamma=1e-1)
 
+#######
+# SIS #
+#######
+
 #Linear spread#
 #SISgRates <- function(x, p, t) {
 #  return(c(x["S"]*(params$beta*x["I"] + params$z), #Infection
@@ -31,8 +35,50 @@ SISg <- function(params,sim_length=10000,S0=1e5,I0=0){
   return(sis_out)
 }
 
+SISg.exact <- function(params,sim_length=10000,S0=1e5,I0=0){
+  init.values <- c(S=S0,I=I0)
+  transitions <-   list(c(S = -1, I = +1), c(I = -1, S = +1))
+  sis_out <- ssa.exact(init.values, transitions, SISgRates, params, tf=sim_length)
+  sis_out <- as.data.frame(sis_out,stringsAsFactors=FALSE)
+  sis_out$B <- rpois(n=length(sis_out$I),lambda=(sis_out$I*params$rho+1e-6))
+  return(sis_out)
+}
+
+
+SISgRates_logistic <- function(x, p, t) {
+  return(c(x["S"]*(params$beta*x["I"]/(1+params$alpha*x["I"]^2) + params$z), #Infection
+           params$gamma*x["I"], #Recovery
+           (params$r*(1-x["S"]/params$K))*x["S"]*(params$r*(1-x["S"]/params$K) > 0), #Density dependent growth
+           -(params$r*(1-x["S"]/params$K))*x["S"]*(params$r*(1-x["S"]/params$K) < 0), #Density-dependent death
+           params$m*x["I"])) #Mortality of diseased individuals
+}
+
+SISg_logistic <- function(params,sim_length=10000,S0=1e5,I0=0){
+  init.values <- c(S=S0,I=I0)
+  transitions <-   list(c(S = -1, I = +1), c(I = -1, S = +1), c(S = +1), c(S = -1), c(I =-1))
+  sis_out <- ssa.adaptivetau(init.values, transitions, SISgRates_logistic, params, tf=sim_length)
+  sis_out <- as.data.frame(sis_out,stringsAsFactors=FALSE)
+  sis_out$B <- rpois(n=length(sis_out$I),lambda=(sis_out$I*params$rho+1e-6))
+  return(sis_out)
+}
+
+
+
+
+########
+# SIRS #
+########
+
+#Linear spread#
+#SIRSgRates <- function(x, p, t) {
+#  return(c(x["S"]*(params$beta*x["I"] + params$z), #Infection rate 
+#           params$gamma*x["I"], # recovery rate
+#           params$mu*x["R"])) #Immune loss rate
+#}
+
+
 SIRSgRates <- function(x, p, t) {
-  return(c(x["S"]*(params$beta*x["I"] + params$z), #Infection rate 
+  return(c(x["S"]*(params$beta*x["I"]/(1+params$alpha*x["I"]^2) + params$z), #Infection rate 
            params$gamma*x["I"], # recovery rate
            params$mu*x["R"])) #Immune loss rate
 }
@@ -45,4 +91,84 @@ SIRSg <- function(params,sim_length=10000,S0=1e5,I0=0,R0=0){
   sirs_out$B <- rpois(n=length(sirs_out$I),lambda=params$rho*sirs_out$I+1e-6)
   return(sirs_out)
 }
+
+
+########################################################################################
+# SIE Model (environmental pool, see Sharp and Pastor 2011 in Ecological Applications) #
+########################################################################################
+
+SIEgRates <- function(x, p, t) {
+  return(c(x["S"]*(params$beta*x["E"] + params$z), #Infection
+           (params$r*(1-x["S"]/params$K))*x["S"]*(params$r*(1-x["S"]/params$K) > 0), #Density dependent growth
+           -(params$r*(1-x["S"]/params$K))*x["S"]*(params$r*(1-x["S"]/params$K) < 0), #Density-dependent death
+           params$m*x["I"], #Mortality of diseased individuals
+           params$epsilon*x["I"],
+           params$tau*x["E"]))
+}
+
+SIEg <- function(params,sim_length=10000,S0=1e5,I0=0,E0=0){
+  init.values <- c(S=S0,I=I0,E=E0)
+  transitions <-   list(c(S = -1, I = +1), c(S = +1), c(S = -1), c(I = -1), c(E = +1), c(E = -1))
+  sis_out <- ssa.adaptivetau(init.values, transitions, SIEgRates, params, tf=sim_length)
+  sis_out <- as.data.frame(sis_out,stringsAsFactors=FALSE)
+  sis_out$B <- rpois(n=length(sis_out$I),lambda=(sis_out$I*params$rho+1e-6))
+  return(sis_out)
+}
+
+
+SIEg.exact <- function(params,sim_length=10000,S0=1e5,I0=0,E0=0){
+  init.values <- c(S=S0,I=I0,E=E0)
+  transitions <-   list(c(S = -1, I = +1), c(S = +1), c(S = -1), c(I = -1), c(E = +1), c(E = -1))
+  sis_out <- ssa.exact(init.values, transitions, SIEgRates, params, tf=sim_length)
+  sis_out <- as.data.frame(sis_out,stringsAsFactors=FALSE)
+  sis_out$B <- rpois(n=length(sis_out$I),lambda=(sis_out$I*params$rho+1e-6))
+  return(sis_out)
+}
+
+
+###########################################################################################
+# Seasonally forced stochastic SIR (Black and McKane 2010 Journal of Theoretical Biology) #
+###########################################################################################
+
+SIRSgRates_forced <- function(x, params, t) {
+  return(c(x["S"]*(params$beta*x["I"]/(1+params$alpha*x["I"]^2) + params$z), #Infection rate 
+           params$gamma*x["I"], # recovery rate
+           params$mu*x["R"])) #Immune loss rate
+}
+
+
+SIRSg_forced <- function(params,sim_iter,on_interval=1,off_interval=1,S0=1e5,I0=0,R0=0){
+  
+  interval_is_on <- 0
+  #sim_iter #Number of cycles
+  #on_interval #length of school year
+  #off_interval  #length of summer break
+  
+  init.values <- c(S=S0,I=I0,R=R0)
+  transitions <-   list(c(S = -1, I = +1), c(I = -1, R = +1), c(R = -1, S = +1))
+  
+  time_counter <- 0
+  sirs_out <- list()
+  for(i in 1:sim_iter){
+    params$beta <- params$beta0*(1+params$beta1*interval_is_on)
+    interval_length = on_interval*interval_is_on + off_interval*(1-interval_is_on)
+    interval_is_on = 1-interval_is_on
+    
+    sirs_out_i <- ssa.adaptivetau(init.values, transitions, SIRSgRates_forced, params, tf=interval_length)
+    sirs_out_i <- as.data.frame(sirs_out_i,stringsAsFactors=FALSE)
+    sirs_out_i$B <- rpois(n=length(sirs_out_i$I),lambda=params$rho*sirs_out_i$I+1e-6)
+    sirs_out_i$time <- sirs_out_i$time + time_counter
+    init.values <- c(S=sirs_out_i$S[length(sirs_out_i$S)],I=sirs_out_i$I[length(sirs_out_i$I)],R=sirs_out_i$R[length(sirs_out_i$R)])
+    sirs_out <- rbind(sirs_out,sirs_out_i)
+    time_counter <- sirs_out$time[length(sirs_out$time)]
+  }
+  return(sirs_out)
+}
+
+
+
+
+
+
+
 
